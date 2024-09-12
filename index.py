58a705b98d0750829a62781f808b4e917a71395a
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-12 15:53:37 +0700

    update

--- index.py
+++ index.py
@@ -5,6 +5,5 @@ import cv2
 import matplotlib.pyplot as plt 
 
 import insightface 
-from insightface.app import FaceAnalyst 
-from insightface.data import get_image as ins_get_image 
+
 print('insightface',insightface.__version__ )
