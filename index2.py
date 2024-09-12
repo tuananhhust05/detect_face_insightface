41e87d3cf55924d8335aafb6969b73052ffaaa0e
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-12 17:30:35 +0700

    update

--- index2.py
+++ index2.py
@@ -5,12 +5,19 @@ import os.path as osp
 import glob
 import cv2
 import insightface
+import matplotlib.plot as plt 
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 
 app = FaceAnalysis('buffalo_l')
 app.prepare(ctx_id = 0, det_size=(640,640))
 img = cv2.imread("test.jpg")
+fig,axs = plt.subplots(1,6,figsize=(12,5))
 faces = app.get(img)
+for i,face in enumerate(faces):
+    bbox = face['box']
+    bbox = [int(b) for b in bbox]
+    filename = f"1test.jpg"
+    cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
 print("faces",len(faces))
 print(faces[0])
