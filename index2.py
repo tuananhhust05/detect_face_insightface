30924be8ae74eae3caa8e3149e300a81cb8dba4c
naimish <makwananaimish@gmail.com>
2024-09-12 23:27:23 -0400

    update

--- index2.py
+++ index2.py
@@ -18,7 +18,7 @@ array_cosin = []
 array_em = []
 app = FaceAnalysis('buffalo_l')
 app.prepare(ctx_id = 0, det_size=(640,640))
-img = cv2.imread("./videotest_frames/frame_11.jpg") 
+img = cv2.imread("./videotest_frames/frame_22.jpg") 
 fig,axs = plt.subplots(1,6,figsize=(12,5))
 faces = app.get(img)
 
@@ -29,7 +29,7 @@ for i,face in enumerate(faces):
     cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
     array_em.append(face['embedding'])
 
-img2 = cv2.imread("./videotest_frames/frame_22.jpg")
+img2 = cv2.imread("./videotest_frames/frame_33.jpg")
 faces2 = app.get(img2)
 for i,face in enumerate(faces2):
     bbox = face['bbox']
@@ -40,9 +40,7 @@ for i,face in enumerate(faces2):
       cosin_value = cosin(em,face['embedding'])
       array_cosin.append(cosin_value)
      #   array_em.append(face['embedding'])
-
+print("first",faces[0])
 print("array_em",array_em)
 print("array_cosin",array_cosin)
-print("first",faces[0])
-
 
