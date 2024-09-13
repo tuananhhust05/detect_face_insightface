188ecb33d9344d0a7f7d5c8dcb3f48898f115180
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-13 10:28:53 +0700

    update

--- index2.py
+++ index2.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 from numpy.linalg import norm
+weight_point = 0.75
 
 def cosin(question,answer):
     cosine = np.dot(question,answer)/(norm(question)*norm(answer))
@@ -18,29 +19,72 @@ array_cosin = []
 array_em = []
 app = FaceAnalysis('buffalo_l')
 app.prepare(ctx_id = 0, det_size=(640,640))
-img = cv2.imread("./videotest_frames/frame_22.jpg") 
+img = cv2.imread("./videotest_frames/frame_11.jpg") 
 fig,axs = plt.subplots(1,6,figsize=(12,5))
-faces = app.get(img)
+# faces = app.get(img)
 
-for i,face in enumerate(faces):
-    bbox = face['bbox']
-    bbox = [int(b) for b in bbox]
-    filename = f"{i}test.jpg"
-    cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
-    array_em.append(face['embedding'])
+# for i,face in enumerate(faces):
+#     bbox = face['bbox']
+#     bbox = [int(b) for b in bbox]
+#     filename = f"{i}test.jpg"
+#     cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
+#     array_em.append(face['embedding'])
 
-img2 = cv2.imread("./videotest_frames/frame_33.jpg")
-faces2 = app.get(img2)
-for i,face in enumerate(faces2):
-    bbox = face['bbox']
-    bbox = [int(b) for b in bbox]
-    filename = f"{i}test.jpg"
-    cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
-    for em in array_em:
-      cosin_value = cosin(em,face['embedding'])
-      array_cosin.append(cosin_value)
+# img2 = cv2.imread("./videotest_frames/frame_22.jpg")
+# faces2 = app.get(img2)
+# for i,face in enumerate(faces2):
+#     bbox = face['bbox']
+#     bbox = [int(b) for b in bbox]
+#     filename = f"{i}test.jpg"
+#     cv2.imwrite('./outputs/%s'%filename,img[bbox[1] : bbox[3], bbox[0]: bbox[2], ::-1])
+#     for em in array_em:
+#       cosin_value = cosin(em,face['embedding'])
+#       array_cosin.append(cosin_value)
      #   array_em.append(face['embedding'])
-print("first",faces[0])
-print("array_em",array_em)
-print("array_cosin",array_cosin)
 
+def extract_frames(video_file):
+    cap = cv2.VideoCapture(video_file)
+    
+    frame_rate = 2  # Desired frame rate (1 frame every 0.5 seconds)
+    frame_count = 0
+    
+    # Get the video file's name without extension
+    video_name = os.path.splitext(os.path.basename(video_file))[0]
+    
+    # Create an output folder with a name corresponding to the video
+    output_directory = f"{video_name}_frames"
+    os.makedirs(output_directory, exist_ok=True)
+    
+   
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+        
+        frame_count += 1
+        
+        # Only extract frames at the desired frame rate
+        if frame_count % int(cap.get(5) / frame_rate) == 0:
+            # output_file = f"{output_directory}/frame_{frame_count}.jpg"
+            # cv2.imwrite(output_file, frame)
+            faces = app.get(frame)
+            for face in faces:
+                if(len(array_em) == 0):
+                    array_em.append(face['embedding'])
+                else:
+                    for em in array_em:
+                        cosin_value = cosin(em,face['embedding'])
+                        print("cosin_value",cosin_value)
+                        if(cosin_value < weight_point):
+                            array_em.append(face['embedding'])
+            
+            # print(f"Frame {frame_count} has been extracted and saved as {output_file}")
+    
+    cap.release()
+    cv2.destroyAllWindows()
+    print("End video")
+
+extract_frames('videotest.mp4')
+print("array_cosin",array_cosin)
+print("array_em",len(array_em))
