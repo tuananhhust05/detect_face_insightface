7338d642595067c465837b8db3c353c2bce79c9a
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-15 09:32:10 +0700

    update

--- index2.py
+++ index2.py
@@ -89,8 +89,9 @@ def extract_frames(video_file):
                 print("soluong  nguoi hien tai", len(array_em))
                 for em in array_em:
                     print("em",em)
-                    print("so phan tu con",len(em["embeddings"])
+                    print("so phan tu con",len(em["embeddings"]))
                     for embed in em["embeddings"]:
+                        print("phan tu con", embed)
                         cosin_value = cosin(embed,face['embedding'])
                         count = count + 1 
                         # print("so lan tinh", count)
