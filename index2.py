ba5213cffc7494006a6519b7101c8239d27e0177
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-17 11:26:36 +0700

    update

--- index2.py
+++ index2.py
@@ -16,7 +16,7 @@ from pinecone import Pinecone, ServerlessSpec
 import uuid
 import json 
 pc = Pinecone(api_key="be4036dc-2d41-4621-870d-f9c4e8958412")
-index = pc.Index("detectface")
+index = pc.Index("detectface2")
 
 weight_point = 0.4
 
