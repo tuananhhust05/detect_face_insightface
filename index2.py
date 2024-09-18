30050f37492b5de0e8bf73b6b77118de45616476
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-18 19:00:50 +0700

    updateupdate

--- index2.py
+++ index2.py
@@ -120,7 +120,7 @@ def extract_frames(video_file):
             # frame = cv2.fastNlMeansDenoisingColored(sharpen,None,10,10,7,21)
             faces = app.get(frame)
             for i,face in enumerate(faces):
-              if(face["det_score"] > 0.85):
+              if(face["det_score"] > 0.8):
                 print("dimension .....")
                 print(face['embedding'].shape)
                 # print(face['embedding'])
