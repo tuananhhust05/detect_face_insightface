edb1f7202d28071f290506d1a833edc4ffd63a0c
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-19 10:09:38 +0700

    update

--- jackma/detectjackma.py
+++ jackma/detectjackma.py
@@ -18,7 +18,7 @@ import json
 import numba
 from numba import jit
 pc = Pinecone(api_key="be4036dc-2d41-4621-870d-f9c4e8958412")
-index = pc.Index("facejackma")
+index = pc.Index("detectface2")
 
 weight_point = 0.4
 
