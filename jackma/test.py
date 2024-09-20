794bb594beb4e60f02822574e5ee4658b39e9dd7
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-20 21:47:01 +0700

    update

--- jackma/test.py
+++ jackma/test.py
@@ -33,7 +33,9 @@ app = FaceAnalysis('buffalo_l')
 app.prepare(ctx_id=0, det_size=(640, 640))  # Ensure InsightFace uses GPU
 list_result = []
 
-def extract_frames(video_file):
+def extract_frames(video_file,index_local):
+    array_em_result = []
+    list_result_ele = []
     frame_count = 0
     frame_rate = 60  #  default 1s with 30 frames
     audio = MP4(video_file)
@@ -68,10 +70,9 @@ def extract_frames(video_file):
                     )
                     matches = search_result["matches"]
 
-                    # print("matches", matches)
                     if len(matches) > 0 and matches[0]['score'] > weight_point:
                         if len(array_em) == 0:
-                            array_em.append({
+                            array_em_result.append({
                                 "speaker": 0,
                                 "frames": [frame_count],
                             })
@@ -85,14 +86,52 @@ def extract_frames(video_file):
                             cv2.imwrite(f'./outputs/{filename}', frame)
                         except Exception as e:
                             print(f"Error saving frame: {e}")
+    
+    for ele in array_em_result:
+        ele["frame_count"] = frame_count
+        ele["duration"] = duration
+        ele["frame_rate"] = frame_rate
+    
+    with open(f"datas/{index_local}.json", 'w') as f:
+       json.dump(array_em_result, f, indent=4)
+    
+    with open(f"datas/{index_local}.json", 'r') as file:
+        data = json.load(file)
+        for em in data:
+            frame_rate = em["frame_rate"] 
+            time_per_frame = em["duration"] / em["frame_count"]
+            list_time_exist = []
+            duration_exist = []
+            list_frame = em["frames"]
+            print("so frame", len(list_frame))
+            for i in range(len(list_frame)-1):
+                if(list_frame[i] == frame_rate):
+                    duration_exist.append(0)
+                duration_exist.append(list_frame[i])
+                if( (list_frame[i + 1] - list_frame[i]) > frame_rate):
+                    list_time_exist.append([duration_exist[0]*time_per_frame,duration_exist[len(duration_exist) - 1] * time_per_frame])
+                    duration_exist = []
+                else:
+                        if( i == len(list_frame)-2):
+                            duration_exist.append(list_frame[i+1])
+                            list_time_exist.append([duration_exist[0]*time_per_frame,duration_exist[len(duration_exist) - 1] * time_per_frame])
+                            duration_exist = []
+            list_result_ele.append({
+                'face':em['speaker'],
+                'duration_exist':list_time_exist
+            })
 
+    with open(f"results/{index_local}.json", 'w') as f:
+        json.dump(list_result_ele, f, indent=4)
+        print("End video")       
+        
     cap.release()
     print("End video")
 
 def process_videos(video_files):
     threads = []
     for i, video_file in enumerate(video_files):
-        t = threading.Thread(target=extract_frames, args=(video_file,))
+        t = threading.Thread(target=extract_frames, args=(video_file,i))
         threads.append(t)
         t.start()
 
