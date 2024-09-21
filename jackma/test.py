99d8536b7916ee9486329167369c240608b69789
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-21 09:45:48 +0700

    update

--- jackma/test.py
+++ jackma/test.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 pc = Pinecone(api_key="be4036dc-2d41-4621-870d-f9c4e8958412")
-index = pc.Index("detectface2")
+index = pc.Index("facejackma")
 
 weight_point = 0.4
 
