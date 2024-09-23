6a7a3052ef88a3c0db103aa84eeaba9248f9ebc1
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-23 10:07:49 +0700

    update

--- checkfile.py
+++ checkfile.py
@@ -1,9 +1,11 @@
 import cv2
 
+frame_count = 0 
 cap = cv2.VideoCapture("/home/ubuntu4080/detect/datacenter/ch02_20240901000000.mp4")
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+    frame_count = frame_count + 1
+    print(frame_count)
     cv2.imwrite("tempt.jpg",frame)
