30f508ea88f9019bb6cf0627fcff5b41196d7380
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-23 10:05:28 +0700

    update

--- checkfile.py
+++ checkfile.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture("ch02_20240901031703.mp4")
+cap = cv2.VideoCapture("/home/ubuntu4080/detect/datacenter/ch02_20240901000000.mp4")
 while True:
     ret, frame = cap.read()
     if not ret:
