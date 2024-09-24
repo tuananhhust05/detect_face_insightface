44080fbc17d0bfe4cb09f1f3a043ed8617bd0ee0
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-24 15:53:21 +0700

    update

--- jackma/detect.py
+++ jackma/detect.py
@@ -318,7 +318,7 @@ print("Start ......",str(start_time))
 f = open("start.txt", "a")
 f.write(str(start_time))
 
-handle_multiplefile(list_file[6:],50)
+handle_multiplefile(list_file,50)
 
 end_time = time.time()
 f = open("end.txt", "a")
