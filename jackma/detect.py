2eb7dd0c0cfbee1bd12e93a3a51468f77789b9d7
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-24 13:38:11 +0700

    update

--- jackma/detect.py
+++ jackma/detect.py
@@ -55,9 +55,11 @@ app = FaceAnalysis('buffalo_l', providers=['CUDAExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(640, 640))  # Ensure InsightFace uses GPU
 list_result = []
 
+def is_model_on_gpu(model):
+    return next(model.parameters()).is_cuda
 for model_name, model in app.models.items():
     print(f"Checking model: {model_name}")
-    # check_model_on_gpu(model)
+    print(model_name,is_model_on_gpu(model))
 
 def extract_frames(folder,video_file,index_local,time_per_segment):
     array_em_result = []
