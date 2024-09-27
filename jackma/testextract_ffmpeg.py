88df66d21cd23934822c1ae5b67111ff2b39197a
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-27 17:22:40 +0700

    update

--- jackma/testextract_ffmpeg.py
+++ jackma/testextract_ffmpeg.py
@@ -1,6 +1,7 @@
 import ffmpeg 
-
-print("start")
+import time 
+print("start", time.time())
  
-ffmpeg.input("2.mp4", hwaccel='cuda', stream_loop=1).filter('fps', fps=60 ).output(f'imgs/frame_%04d.png', format='image2').run(overwrite_output=True)
+ffmpeg.input("2.mp4", hwaccel='cuda', stream_loop=1).filter('fps', fps=0.5  ).output(f'imgs/frame_%04d.png', format='image2').run(overwrite_output=True)
 print("finish")
+print("end", time.time()) 
