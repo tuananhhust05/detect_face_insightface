bff47a1a748e5933961dba7c4473635c8d5882c4
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 06:59:27 +0700

    update

--- new_solution/Detector.py
+++ new_solution/Detector.py
@@ -7,7 +7,7 @@ class Detector:
         self.faceModel = cv2.dnn.readNetFromCaffe("res10_300x300_ssd_iter_140000.prototxt",
             caffeModel="res10_300x300_ssd_iter_140000.caffemodel")
     
-    def processImag(self, imgName):
+    def processImage(self, imgName):
         self.img = cv2.imread(imgName)
         (self.height, self.width) = self.img.shape[:2]
         
