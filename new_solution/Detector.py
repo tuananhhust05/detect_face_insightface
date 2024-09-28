fbd19dd9661f4f9e3beaabf5254c831f8717ddb5
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 07:06:01 +0700

    update

--- new_solution/Detector.py
+++ new_solution/Detector.py
@@ -12,7 +12,7 @@ class Detector:
         (self.height, self.width) = self.img.shape[:2]
 
         self.processFrame()
-        
+
         
     def processFrame(self):
         blob = cv2.dnn.blobFromImage(self.img, 1.0, (300,300), (104.0, 177.0, 123.0), swapRB = False, crop = False)
@@ -24,4 +24,4 @@ class Detector:
             if predictions[0, 0, i, 2] > 0.5 :
                 bbox = predictions[0,0,i,3:7]   * np.array([self.width, self.height,self.width, self.height] )
                 (xmin, ymin, xmax, ymax) = bbox.astype("int")
-                cv2.retangle(self.img, (xmin, ymin), (xmax,ymax), (0,0,255), 2 )
+                cv2.rectangle(self.img, (xmin, ymin), (xmax,ymax), (0,0,255), 2 )
