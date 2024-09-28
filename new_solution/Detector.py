778c2688790ec49233ac7696dfc50f6f49d9551b
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 10:11:13 +0700

    update

--- new_solution/Detector.py
+++ new_solution/Detector.py
@@ -25,9 +25,33 @@ class Detector:
         # print("predictions", predictions)
         for i in range(0, predictions.shape[2]):
             if predictions[0, 0, i, 2] > 0.5 :
-                # bbox = predictions[0,0,i,3:7]   * np.array([self.width, self.height,self.width, self.height] )
-                # (xmin, ymin, xmax, ymax) = bbox.astype("int")
-                self.result =  True
-                return
+                bbox = predictions[0,0,i,3:7]   * np.array([self.width, self.height,self.width, self.height] )
+                (xmin, ymin, xmax, ymax) = bbox.astype("int")
+                # self.result =  True
+                # return
+            
+    def processVideo(self, videoName):
+        count = 0 
+        cap = cv2.VideoCapture(videoName)
+        if(cap.isOpened() == False):
+            print("error")
+        
+        (sucess, self.img) = cap.read()
+        (self.height, self.width)  = self.img.shape[:2]
+
+        fps = FPS.start()
+        while sucess:
+            if(count % 60 == 0):
+                self.processFrame()
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+              break
+            (sucess,self.img) = cap.read()
+            count = count + 1 
+            print(count)
+            
+        fps.stop()
+
+        cap.release()
           
             
--- new_solution/main.py
+++ new_solution/main.py
@@ -1,6 +1,9 @@
 from Detector import * 
-
+import time 
 detector = Detector()
 
-check = detector.processImage("jackma.png")
+start = time.time()
+check = detector.processVideo("/home/poc4a5000/facesx/ch02_20240904040117.mp4")
+end = time.time()
+print("ecution", end - start)
 print(detector.result)
