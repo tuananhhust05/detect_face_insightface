aed6c1140b78c3fa375cccc7f206933b2fbe4784
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 08:31:21 +0700

    update

--- jackma/main.py
+++ jackma/main.py
@@ -1,6 +1,6 @@
 from Detector import * 
 import cv2
 detector = Detector()
-img = cv2.imread("jackma.png")
+img = cv2.imread("10740_0_output.jpg")
 check = detector.processImage(img)
 print(detector.result)
