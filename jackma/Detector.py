a7e58f8555d799142c82032d3fa446e7da91352e
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 07:36:11 +0700

    update

--- jackma/Detector.py
+++ jackma/Detector.py
@@ -6,7 +6,7 @@ class Detector:
     def __init__(self):
         self.faceModel = cv2.dnn.readNetFromCaffe("res10_300x300_ssd_iter_140000.prototxt",
             caffeModel="res10_300x300_ssd_iter_140000.caffemodel")
-    
+        self.result = False
     def processImage(self, imgName):
         self.img = cv2.imread(imgName)
         (self.height, self.width) = self.img.shape[:2]
