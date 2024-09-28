3ff6baf7125479be607fa3b3727e951d6bda8733
tuananhhust05 <tuananhhust05@gmail.com>
2024-09-28 11:43:35 +0700

    update

--- new_solution/Detector.py
+++ new_solution/Detector.py
@@ -18,17 +18,25 @@ class Detector:
         
     def processFrame(self):
         self.result = False
-        blob = cv2.dnn.blobFromImage(self.img, 1.0, (300,300), (104.0, 177.0, 123.0), swapRB = False, crop = False)
+        
+        # Convert the image to a CUDA GpuMat
+        gpu_img = cv2.cuda_GpuMat()
+        gpu_img.upload(self.img)
 
+        # Prepare the blob from the GPU image
+        blob = cv2.cuda.dnn.blobFromImage(gpu_img, 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+        # Set the input blob for the model
         self.faceModel.setInput(blob)
         predictions = self.faceModel.forward()
-        # print("predictions", predictions)
-        for i in range(0, predictions.shape[2]):
-            if predictions[0, 0, i, 2] > 0.5 :
-                bbox = predictions[0,0,i,3:7]   * np.array([self.width, self.height,self.width, self.height] )
+        
+        for i in range(predictions.shape[2]):
+            if predictions[0, 0, i, 2] > 0.5:
+                bbox = predictions[0, 0, i, 3:7] * np.array([self.width, self.height, self.width, self.height])
                 (xmin, ymin, xmax, ymax) = bbox.astype("int")
-                # self.result =  True
-                # return
+                # Here you can draw the bounding box or perform other actions
+                cv2.rectangle(self.img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                self.result = True
             
     def processVideo(self, videoName):
         count = 0 
