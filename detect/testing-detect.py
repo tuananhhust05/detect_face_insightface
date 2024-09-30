0ab2b03a25fa95a8b1c51a953f419c12184d0770
Truong Duy <cutruongduy97@gmail.com>
2024-09-30 16:33:36 +0700

    debugging

--- detect/testing-detect.py
+++ detect/testing-detect.py
@@ -32,7 +32,7 @@ videos = mydb["videos"]
 dir_project = "/home/poc4a5000/detect/detect"
 
 # Pinecone initialization
-api_key = "6bebb6ba-195f-471e-bb60-e0209bd5c697"
+api_key = "YOUR_PINECONE_API_KEY"  # Replace with your Pinecone API key
 if not api_key:
     logging.error("Pinecone API key not found.")
     raise ValueError("Pinecone API key not found.")
@@ -74,137 +74,121 @@ def current_date():
     date_string = now.strftime(format_date)
     return datetime.datetime.strptime(date_string, format_date)
 
-# Batch processing function
-def process_batch(frames_batch, frame_indices, folder, video_file, index_local, time_per_segment, case_id, duration, total_frames, gpu_id):
+# Function to process frames one by one
+def extract_frames(folder, video_file, index_local, time_per_segment, case_id, gpu_id):
+    logging.info(f"Process {current_process().name} started with GPU ID: {gpu_id}")
     try:
-        logging.info(f"Process {current_process().name} processing batch on GPU {gpu_id}")
-        
         # Set the device
+        device = torch.device(f'cuda:{gpu_id}' if torch.cuda.is_available() else 'cpu')
         torch.cuda.set_device(gpu_id)
-        device_str = f'cuda:{gpu_id}'
 
-        # Initialize FaceAnalysis and model in this process
-        face_analysis = FaceAnalysis('buffalo_l')
+        # Ensure that the providers include CUDAExecutionProvider
+        providers = ['CUDAExecutionProvider'] if torch.cuda.is_available() else ['CPUExecutionProvider']
+
+        # Initialize FaceAnalysis and model in this process with GPU providers
+        face_analysis = FaceAnalysis(
+            name='buffalo_l',
+            providers=providers,
+            allowed_modules=['detection', 'recognition']
+        )
         face_analysis.prepare(ctx_id=gpu_id, det_size=(640, 640))
-        model = model_zoo.get_model('/home/poc4a5000/.insightface/models/buffalo_l/det_10g.onnx')
+        model = model_zoo.get_model(
+            '/home/poc4a5000/.insightface/models/buffalo_l/det_10g.onnx',
+            providers=providers
+        )
         model.prepare(ctx_id=gpu_id, det_size=(640, 640))
-        
-        for frame, frame_count in zip(frames_batch, frame_indices):
-            detections, _ = model.detect(frame, input_size=(640, 640))
-            if detections is not None and len(detections) > 0:
-                sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-                sharpen = cv2.filter2D(frame, -1, sharpen_kernel)
-                denoised = cv2.fastNlMeansDenoisingColored(sharpen, None, 10, 10, 7, 21)
-                faces = face_analysis.get(denoised)
 
-                sum_age = 0 
-                sum_gender = 0 
-                count_face = 0 
-
-                for face in faces:
-                    if face["det_score"] > 0.5:
-                        embedding = torch.tensor(face['embedding']).to(device_str)
-                        search_result = index.query(
-                            vector=embedding.cpu().numpy().tolist(),
-                            top_k=1,
-                            include_metadata=True,
-                            include_values=True,
-                            filter={"face": case_id},
-                        )
-                        matches = search_result["matches"]
-
-                        if len(matches) > 0 and matches[0]['score'] > weight_point:
-                            count_face += 1 
-                            sum_age += int(face['age'])
-                            sum_gender += int(face['gender'])
-
-                            # Record the result
-                            mydict = { 
-                                "id":  str(uuid.uuid4()), 
-                                "case_id": case_id,
-                                "similarity_face":str(matches[0]['score']),
-                                "gender":int(face['gender']),
-                                "age":int(face['age']),
-                                "time_invideo":str(frame_count * time_per_frame_global + time_per_segment * index_local),
-                                "proofImage":f'/home/poc4a5000/detect/detect/faces/{case_id}/{folder}/{index_local}/{frame_count}_0_face.jpg',
-                                "url":f'/home/poc4a5000/detect/detect/faces/{case_id}/{folder}/{index_local}/{frame_count}_0_face.jpg',
-                                "createdAt":current_date(),
-                                "updatedAt":current_date(),
-                                "file":folder
-                            }
-                            facematches.insert_one(mydict)
-
-                            # Draw rectangle around the face
-                            bbox = [int(b) for b in face['bbox']]
-                            filename = f"{frame_count}_0_face.jpg"
-                            face_dir = f"./faces/{case_id}/{folder}/{index_local}"
-                            output_dir = f"./outputs/{case_id}/{folder}/{index_local}"
-                            os.makedirs(face_dir, exist_ok=True)
-                            os.makedirs(output_dir, exist_ok=True)
-
-                            cv2.imwrite(f'{face_dir}/{filename}', frame[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
-
-                            top_left = (bbox[0], bbox[1])
-                            bottom_right = (bbox[2], bbox[3])
-                            color = (255, 0, 0)
-                            thickness = 2
-                            cv2.rectangle(frame, top_left, bottom_right, color, thickness)
-                            time_in_video = frame_count * time_per_frame_global + time_per_segment * index_local
-                            text = str(time_in_video)
-                            position = (bbox[0], bbox[1])
-                            font = cv2.FONT_HERSHEY_SIMPLEX
-                            font_scale = 1
-
-                            cv2.putText(frame, text, position, font, font_scale, color, thickness)
-                            cv2.imwrite(f'{output_dir}/{filename}', frame)
-
-    except Exception as e:
-        logging.error(f"Error processing batch in {current_process().name}: {e}")
-    finally:
-        torch.cuda.empty_cache()
-
-# Video processing function
-def process_video(folder, video_file, index_local, time_per_segment, case_id, duration, total_frames, gpu_id):
-    try:
-        logging.info(f"{current_process().name}: Starting video processing on GPU {gpu_id}")
         frame_count = 0
+        duration = getduration(video_file)
         cap = cv2.VideoCapture(video_file)
         fps = cap.get(cv2.CAP_PROP_FPS)
         if fps == 0:
             logging.error(f"{current_process().name}: FPS is zero for video {video_file}")
             cap.release()
             return
-        frame_rate = int(time_per_frame_global * fps) 
-        logging.info(f"{current_process().name}: Video FPS: {fps}, Frame rate for processing: {frame_rate}")
-
-        frames_batch = []
-        frame_indices = []
-        batch_size = 16  # Adjust batch size as needed
+        frame_rate = int(fps)  # Adjust as needed
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        logging.info(f"{current_process().name}: Video FPS: {fps}, Total frames: {total_frames}")
 
         while True:
             ret, frame = cap.read()
             if not ret:
-                logging.info(f"{current_process().name}: No more frames to read, breaking the loop.")
                 break
             frame_count += 1
+
             if frame_count % frame_rate == 0:
-                logging.info(f"{current_process().name}: Adding frame {frame_count} to batch")
-                frames_batch.append(frame)
-                frame_indices.append(frame_count)
-                if len(frames_batch) == batch_size:
-                    logging.info(f"{current_process().name}: Batch size reached, calling process_batch")
-                    process_batch(frames_batch, frame_indices, folder, video_file, index_local, time_per_segment, case_id, duration, total_frames, gpu_id)
-                    frames_batch = []
-                    frame_indices = []
-        if frames_batch:
-            logging.info(f"{current_process().name}: Processing remaining frames in batch")
-            process_batch(frames_batch, frame_indices, folder, video_file, index_local, time_per_segment, case_id, duration, total_frames, gpu_id)
+                logging.info(f"{current_process().name}: Processing frame {frame_count}")
+
+                # Convert frame to appropriate format
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                detections, _ = model.detect(frame_rgb, input_size=(640, 640))
+                if detections is not None and len(detections) > 0:
+                    # Preprocessing
+                    sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+                    sharpen = cv2.filter2D(frame_rgb, -1, sharpen_kernel)
+                    denoised = cv2.fastNlMeansDenoisingColored(sharpen, None, 10, 10, 7, 21)
+
+                    faces = face_analysis.get(denoised)
+                    for face in faces:
+                        if face["det_score"] > 0.5:
+                            embedding = torch.tensor(face['embedding']).to(device)
+                            search_result = index.query(
+                                vector=embedding.cpu().numpy().tolist(),
+                                top_k=1,
+                                include_metadata=True,
+                                include_values=True,
+                                filter={"face": case_id},
+                            )
+                            matches = search_result["matches"]
+
+                            if len(matches) > 0 and matches[0]['score'] > weight_point:
+                                # Record the result
+                                time_in_video = frame_count * (duration / total_frames) + time_per_segment * index_local
+                                bbox = [int(b) for b in face['bbox']]
+                                filename = f"{frame_count}_0_face.jpg"
+                                face_dir = f"./faces/{case_id}/{folder}/{index_local}"
+                                output_dir = f"./outputs/{case_id}/{folder}/{index_local}"
+                                os.makedirs(face_dir, exist_ok=True)
+                                os.makedirs(output_dir, exist_ok=True)
+
+                                # Save the face image
+                                face_image = frame_rgb[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+                                cv2.imwrite(f'{face_dir}/{filename}', cv2.cvtColor(face_image, cv2.COLOR_RGB2BGR))
+
+                                # Draw rectangle and text on the frame
+                                top_left = (bbox[0], bbox[1])
+                                bottom_right = (bbox[2], bbox[3])
+                                color = (255, 0, 0)
+                                thickness = 2
+                                cv2.rectangle(frame_rgb, top_left, bottom_right, color, thickness)
+
+                                text = str(time_in_video)
+                                position = (bbox[0], bbox[1])
+                                font = cv2.FONT_HERSHEY_SIMPLEX
+                                font_scale = 1
+                                cv2.putText(frame_rgb, text, position, font, font_scale, color, thickness)
+                                cv2.imwrite(f'{output_dir}/{filename}', cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR))
+
+                                mydict = {
+                                    "id":  str(uuid.uuid4()),
+                                    "case_id": case_id,
+                                    "similarity_face": str(matches[0]['score']),
+                                    "gender": int(face['gender']),
+                                    "age": int(face['age']),
+                                    "time_invideo": text,
+                                    "proofImage": os.path.abspath(f'{face_dir}/{filename}'),
+                                    "url": os.path.abspath(f'{face_dir}/{filename}'),
+                                    "createdAt": current_date(),
+                                    "updatedAt": current_date(),
+                                    "file": folder
+                                }
+                                facematches.insert_one(mydict)
+
         cap.release()
-        logging.info(f"{current_process().name}: Finished video processing")
+        logging.info(f"{current_process().name}: Finished processing video {video_file}")
     except Exception as e:
-        logging.error(f"{current_process().name}: Exception in process_video: {e}")
-    finally:
-        cap.release()
+        logging.error(f"Error in {current_process().name}: {e}")
 
 # Function to trim video into segments
 def trimvideo(folder, videofile, count_thread, case_id):
@@ -220,17 +204,22 @@ def trimvideo(folder, videofile, count_thread, case_id):
         subprocess.run(command, shell=True, check=True)
 
 # Worker process function
-def worker_process(gpu_id, folder, video_file, index_local, time_per_segment, case_id, duration, total_frames):
+def worker_process(gpu_id, folder, video_file, index_local, time_per_segment, case_id):
     logging.info(f"Process {current_process().name} started with GPU ID: {gpu_id}")
-    # Set the device for this process
-    torch.cuda.set_device(gpu_id)
-    process_video(folder, video_file, index_local, time_per_segment, case_id, duration, total_frames, gpu_id)
+    extract_frames(folder, video_file, index_local, time_per_segment, case_id, gpu_id)
 
 # Function to process target images
 def target_processing(case_id, target_folder, gpu_id):
     torch.cuda.set_device(gpu_id)
-    device_str = f'cuda:{gpu_id}'
-    app_recognize = FaceAnalysis('buffalo_l')
+    device = torch.device(f'cuda:{gpu_id}' if torch.cuda.is_available() else 'cpu')
+
+    providers = ['CUDAExecutionProvider'] if torch.cuda.is_available() else ['CPUExecutionProvider']
+
+    app_recognize = FaceAnalysis(
+        name='buffalo_l',
+        providers=providers,
+        allowed_modules=['detection', 'recognition']
+    )
     app_recognize.prepare(ctx_id=gpu_id, det_thresh=0.3, det_size=(640, 640))
     logging.info(f"app_recognize initialized on GPU {gpu_id}")
 
@@ -275,7 +264,6 @@ def handle_multiplefile(listfile, thread, case_id):
             logging.error(f"Video duration is zero for file {file}")
             continue
         time_per_segment = duration / thread
-        total_frames = int(cv2.VideoCapture(file).get(cv2.CAP_PROP_FRAME_COUNT))
 
         # Trim video into segments
         trimvideo(folder_name, file, thread, case_id)
@@ -284,7 +272,7 @@ def handle_multiplefile(listfile, thread, case_id):
 
         for i, vf in enumerate(video_files):
             gpu_id = video_gpu_ids[process_count % len(video_gpu_ids)]  # Round-robin GPU assignment per process
-            p = Process(target=worker_process, args=(gpu_id, folder_name, vf, i, time_per_segment, case_id, duration, total_frames))
+            p = Process(target=worker_process, args=(gpu_id, folder_name, vf, i, time_per_segment, case_id))
             p.start()
             processes.append(p)
             process_count += 1  # Increment process counter
@@ -304,8 +292,8 @@ def handle_main(case_id, tracking_folder, target_folder):
 
     # List of files to process
     list_file = [
-        os.path.join(tracking_folder, f) 
-        for f in os.listdir(tracking_folder) 
+        os.path.join(tracking_folder, f)
+        for f in os.listdir(tracking_folder)
         if os.path.isfile(os.path.join(tracking_folder, f))
     ]
     handle_multiplefile(list_file, 8, case_id)
@@ -321,7 +309,7 @@ def analyst():
     case_id = request.json.get('case_id')
     tracking_folder = request.json.get('tracking_folder')
     target_folder = request.json.get('target_folder')
-    
+
     if not all([case_id, tracking_folder, target_folder]):
         return jsonify({"error": "Missing parameters."}), 400
 
