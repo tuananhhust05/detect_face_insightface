696336c115b005480e169e3369a93cba25722355
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-01 18:36:41 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -134,6 +134,13 @@ def extract_frames(folder,video_file,index_local,time_per_segment,case_id,gpu_id
         if frame_count % frame_rate == 0:
             print("frame_count", frame_count)
             
+            gpu_frame = cv2.cuda_GpuMat()
+            gpu_frame.upload(frame)
+            
+            # Now the frame is on GPU memory. You can perform GPU-based processing here.
+
+            # For demonstration: download it back to CPU and show it
+            frame = gpu_frame.download()
             # facechecks = model.detect(frame,input_size=(640, 640))
             facechecks = list_model_detect[gpu_id].detect(frame,input_size=(640, 640))
             flagDetect = False
@@ -529,7 +536,7 @@ def handle_main(case_id, tracking_folder, target_folder):
             full_path = f"{tracking_folder}/{path}"
             list_file.append(full_path)
     if(len(list_file) > 0):
-        handle_multiplefile(list_file,150,case_id)
+        handle_multiplefile(list_file,50,case_id)
 
     return 
 
