ca80042bb352edf36383e7b3b7a6eb672088f9cc
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-02 01:24:34 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -395,7 +395,7 @@ def create_video_apperance(case_id,thread_count,folder):
     out.release()
     
     subprocess.run(f"ffmpeg -i {outputpathpre} -codec:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p {output} -y", shell=True, check=True)
-    subprocess.run(f"rm -rf {outputpathpre}", shell=True, check=True)
+    # subprocess.run(f"rm -rf {outputpathpre}", shell=True, check=True)
     
 
 
@@ -408,7 +408,7 @@ def create_video_apperance(case_id,thread_count,folder):
        f.write(f"file '{outputfinal}'\nfile '{output}' ")
        subprocess.run(f"ffmpeg -f concat -safe 0 -i {dir_project}/video_apperance/{case_id}/tempt.txt -c copy {outputfinal}", shell=True, check=True)
     
-    subprocess.run(f"rm -rf {output}", shell=True, check=True)
+    # subprocess.run(f"rm -rf {output}", shell=True, check=True)
 
     videos.insert_one({
         "id":str(uuid.uuid4()),
