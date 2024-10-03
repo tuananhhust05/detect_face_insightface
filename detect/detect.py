1fba73987c8acbd5380116ed9cbf3360c9413a4f
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-03 22:25:39 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -45,7 +45,7 @@ pc = Pinecone(api_key="be4036dc-2d41-4621-870d-f9c4e8958412")
 index = pc.Index("detectcamera")
 
 weight_point = 0.4
-time_per_frame_global = 3 
+time_per_frame_global = 1 
 ctx_id = 0 if device.type == 'cuda' else -1
 app_recognize = FaceAnalysis('buffalo_l',providers=['CUDAExecutionProvider'])
 app_recognize.prepare(ctx_id=ctx_id, det_thresh=0.3, det_size=(640, 640))
