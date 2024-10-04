e8f20e95f6ba6f0687bdd48293a30ce71851649d
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-04 19:24:14 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -705,7 +705,7 @@ def handle_other_face():
                                 for face_change in list_vector_other:
                                     if(face_change["face_id"] == face_compare["face_id"]):
                                         face_change["face_id"] = face["face_id"]
-                # if "embedding" in face:
+                # if "embedding" in  face:
                 #     insert_to_database(face)
                 #     list_inserted.append(face["id"])
 
