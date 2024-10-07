3ef914903e43f82990af36da7b5adb73ae32bbae
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-07 23:37:11 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -608,14 +608,14 @@ def create_video_apperance(case_id,thread_count,folder):
 
 def cutvideo(videofile,start,duration,output):
     (
-        # ffmpeg
-        # .input(videofile, ss=start, hwaccel='cuda')
-        # .output(output, t=duration, c='copy')
-        # .run(overwrite_output=True)
         ffmpeg
-            .input(videofile, ss=start, hwaccel='cuda')
-            .output(output, t=duration, vf=f'scale=640:640', vcodec='h264_nvenc', acodec='copy')
-            .run(overwrite_output=True)
+        .input(videofile, ss=start, hwaccel='cuda')
+        .output(output, t=duration, c='copy')
+        .run(overwrite_output=True)
+        # ffmpeg
+        #     .input(videofile, ss=start, hwaccel='cuda')
+        #     .output(output, t=duration, vf=f'scale=640:640', vcodec='h264_nvenc', acodec='copy')
+        #     .run(overwrite_output=True)
     )
     
 
@@ -638,9 +638,9 @@ def cutvideo(videofile,start,duration,output):
         # )
         # .run(overwrite_output=True)
         # ffmpeg -ss 00:01:00 -i input.mp4 -t 00:02:00 -vf "scale=426:240" -c:v libx264 -preset fast -crf 23 -c:a aac -b:a 128k output.mp4
-    print(f"ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=640:640,pad=640:640:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y")
-    # subprocess.run(f"sudo docker run --rm -it --gpus all -e NVIDIA_DRIVER_CAPABILITIES=video,compute,utility --volume $PWD:/workspace willprice/nvidia-ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=720:720,pad=720:720:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y", shell=True, check=True)
-    subprocess.run(f"ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=640:640,pad=640:640:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y", shell=True, check=True)
+    # print(f"ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=640:640,pad=640:640:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y")
+    # # subprocess.run(f"sudo docker run --rm -it --gpus all -e NVIDIA_DRIVER_CAPABILITIES=video,compute,utility --volume $PWD:/workspace willprice/nvidia-ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=720:720,pad=720:720:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y", shell=True, check=True)
+    # subprocess.run(f"ffmpeg -hwaccel cuda -ss {start} -i {videofile} -vf \"scale=640:640,pad=640:640:(ow-iw)/2:(oh-ih)/2\" -t {duration} -c:v h264_nvenc -preset fast -b:v 5M {output} -y", shell=True, check=True)
     
     # )
 
