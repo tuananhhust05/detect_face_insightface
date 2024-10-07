c8e145b0f7af228e729c3b50f73e5491a46c9fc2
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-07 23:31:00 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -930,7 +930,7 @@ def handle_main(case_id, tracking_folder, target_folder):
                 full_path = f"{tracking_folder}/{path}"
                 list_file.append(full_path)
         if(len(list_file) > 0):
-            handle_multiplefile(list_file,80,case_id)
+            handle_multiplefile(list_file,40,case_id)
             cases.update_many({
                 "id":case_id
             },{
