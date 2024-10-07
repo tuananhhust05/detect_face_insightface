6adf1349ab2001d55ff54a5a1e73ceae9a92b296
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-08 01:41:31 +0700

    update

--- detect/test.py
+++ detect/test.py
@@ -9,7 +9,7 @@ ctx_id = 0 if device.type == 'cuda' else  -1
 appmain = FaceAnalysis('buffalo_l',providers=['CUDAExecutionProvider'])
 appmain.prepare(ctx_id=ctx_id, det_thresh=0.5, det_size=(640, 640))
 
-img = cv2.imread("/home/ubuntua5000/detect/detect/outputs/db696a35-0043-4aba-a844-295e3432a118/ch02_20240904131243_000_640x640/4/672_0_face.jpg")
+img = cv2.imread("/home/ubuntua5000/detect/detect/outputs/db696a35-0043-4aba-a844-295e3432a118/ch02_20240904131243_000_640x640/15/1575_0_face.jpg")
 
 faces = appmain.get(img)
 
