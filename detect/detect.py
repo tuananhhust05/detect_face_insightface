13aafabec9c6ae1a97bbf338056d8b068a12ac8c
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-08 09:29:11 +0700

    update

--- detect/detect.py
+++ detect/detect.py
@@ -677,7 +677,8 @@ def trimvideo(folder,videofile,count_thread,case_id):
     name_file = file.split(".")[0]
     new_name = f"{name_file}_tempt"
     new_path = origin_videofile.replace(name_file,new_name)
-
+    
+    print(f"ffmpeg -i {videofile} -c:v copy -c:a copy {new_path} -y && rm {videofile} && mv {new_path} {videofile}")
     subprocess.run(f"ffmpeg -i {videofile} -c:v copy -c:a copy {new_path} -y && rm {videofile} && mv {new_path} {videofile}", shell=True, check=True)
     for i in range(count_thread):
         t = threading.Thread(target=cutvideo, args=(videofile,time_per_segment*i,time_per_segment,f"/home/ubuntua5000/detect/detect/videos/{case_id}/{folder}/{i}.mp4"))
