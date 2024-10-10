864f57e81297cf3e0c29066b1e0905f2637eb254
tuananhhust05 <tuananhhust05@gmail.com>
2024-10-10 11:54:36 +0700

    update

--- new_solution/main.py
+++
@@ -1,9 +0,0 @@
-from Detector import * 
-import time 
-detector = Detector()
-
-start = time.time()
-check = detector.processVideo("/home/poc4a5000/facesx/ch02_20240904040117.mp4")
-end = time.time()
-print("ecution", end - start)
-print(detector.result)
